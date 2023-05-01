f45621777969dcfdd3cb8c51f97873b2ece7f654
BraeTroutman <braetroutman@gmail.com>
2023-04-30 20:55:28 -0400

    add spatially bounded construct function

--- src/Edist.jl
+++ src/Edist.jl
@@ -184,6 +184,123 @@ module Hirschberg
 end # module Hirschberg
 
 module Bounded
+    import ..Full
+    
+    function score(sequence::String, query::String)
+        return construct(sequence, query)[3][end,end]
+    end
+
+    function construct(sequence::String, query::String)
+        M = length(sequence)
+        N = length(query)
+        
+        k = 3
+        d = abs(M - N)
+
+        if M > N
+            return construct(query, sequence, k, d)  
+        else
+            return construct(sequence, query, k, d)
+        end
+    end
+
+    function construct(sequence::String, query::String, k::Int, d::Int)
+        # assume that the query is longer than the sequence
+        M = length(sequence)+1
+        N = length(query)+1
+        
+        top_left = Full.construct(sequence[1:k-1], query[1:k+d-1])
+        right_frontier = zeros(Int64, M-k, k-1)
+        bottm_frontier = zeros(Int64, M-k, d+k)
+
+        # calculate the first right frontier
+        match = (query[k+d] == sequence[1]) ? 1 : -1
+        right_frontier[1,1] = max(
+                                   top_left[1, k+d] + match,
+                                   top_left[2, k+d] - 2
+                                  )
+        for i in 2:k-1
+            match = (query[k+d] == sequence[i]) ? 1 : -1
+            right_frontier[1,i] = max(
+                                      top_left[i+1, k+d] - 2,
+                                      right_frontier[1,i-1] -2,
+                                      top_left[i, k+d] + match
+                                     )
+        end
+
+        # calculate the first bottom frontier
+        match = (query[1] == sequence[k]) ? 1 : -1
+        bottm_frontier[1,1] = max(
+                                  top_left[k,1] + match,
+                                  top_left[k,2] - 2
+                                 )
+        for i in 2:d+k-1
+            match = (query[i] == sequence[k]) ? 1 : -1
+            bottm_frontier[1,i] = max(
+                                      top_left[k, i] + match,
+                                      top_left[k, i+1] - 2,
+                                      bottm_frontier[1, i-1] - 2
+                                     )
+        end
+        match = (query[d+k] == sequence[k]) ? 1 : -1
+        bottm_frontier[1,end] = max(
+                                    top_left[end,end] + match,
+                                    bottm_frontier[1,end-1] - 2,
+                                    right_frontier[1,end] - 2
+                                   )
+
+        for f in 2:M-k
+            # right frontier 
+            # special case-- first element in frontier has no element directly above
+            match = (query[d+k+f-1] == sequence[f]) ? 1 : -1
+            right_frontier[f, 1] = max(
+                                              right_frontier[f-1, 1] + match,
+                                              right_frontier[f-1, 2] - 2
+                                             )
+            # generic case
+            for i in 2:k-2
+                match = (query[d+k+f-1] == sequence[f+i-1]) ? 1 : -1
+                right_frontier[f, i] = max(
+                                                  right_frontier[f-1, i] + match,
+                                                  right_frontier[f-1, i+1] - 2,
+                                                  right_frontier[f, i-1] - 2
+                                                 )
+            end
+            # special case-- last element in frontier must access last element in bottom frontier
+            match = (query[d+k+f-1] == sequence[f+k-2]) ? 1 : -1
+            right_frontier[f, end] = max(
+                                                right_frontier[f-1, end] + match,
+                                                bottm_frontier[f-1, end] - 2,
+                                                right_frontier[f, end-1] - 2
+                                               )
+            # bottom frontier
+            # special case-- first element in frontier has no element directly to left
+            match = (query[f] == sequence[k+f-1]) ? 1 : -1
+            bottm_frontier[f, 1] = max(
+                                       bottm_frontier[f-1, 1] + match,
+                                       bottm_frontier[f-1, 2] - 2
+                                      )
+
+            # generic case
+            for i in 2:d+k-1
+                match = (query[f+i-1] == sequence[k+f-1]) ? 1 : -1
+                bottm_frontier[f, i] = max(
+                                           bottm_frontier[f-1, i] + match,
+                                           bottm_frontier[f-1, i+1] - 2,
+                                           bottm_frontier[f, i-1] - 2
+                                          )
+            end
+
+            # special case-- last element in frontier must access last element of right_frontier
+            match = (query[f+d+k-1] == sequence[k+f-1]) ? 1 : -1
+            bottm_frontier[f,end] = max(
+                                        bottm_frontier[f-1, end] + match,
+                                        right_frontier[f, end] - 2,
+                                        bottm_frontier[f, end-1] - 2
+                                       )
+        end
+        return top_left, right_frontier, bottm_frontier
+    end
 
 end # module Bounded
 
